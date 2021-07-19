0914f865234f3d5641adc03c9dcee77f2e8fcefb
pawan941394 <63099276+pawan941394@users.noreply.github.com>
2021-07-19 05:48:11 +0530

    Add files via upload

---
+++ 4.Profile UI Card/index.js
@@ -0,0 +1,14 @@
+let content = document.getElementById("con");
+let img = document.getElementById("img");
+
+
+function hover() {
+    img.style.display = "none";
+    content.style.display = "block";
+
+}
+
+function out() {
+    img.style.display = "block";
+    content.style.display = "none";
+}
